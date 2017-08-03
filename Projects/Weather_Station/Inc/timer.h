c8a31c5a762b5fd41692e8e7abccf1a08167ffa0
AdamBerczi <adamberczi90@gmail.com>
2017-08-03 10:29:48 +0200

    add weather station files

---
+++ Projects/Weather_Station/Inc/timer.h
@@ -0,0 +1,16 @@
+/*
+ * timer.h
+ *
+ *  Created on: Aug 2, 2017
+ *      Author: Adam
+ */
+
+#ifndef EXAMPLE_USER_TIMER_H_
+#define EXAMPLE_USER_TIMER_H_
+
+#include "stm32l4xx_hal.h"
+#include "stm32l475e_iot01.h"
+
+
+
+#endif /* EXAMPLE_USER_TIMER_H_ */
