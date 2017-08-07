e916481dc5e1056b8cc96c9af472af08b8d63982
Mate Lengyel <lengyel.mate@gmail.com>
2017-08-07 17:31:46 +0200

    going forward

--- Projects/ac_decoder/main.c
+++ Projects/ac_decoder/main.c
@@ -11,12 +11,12 @@ FILE* files[15];
 int main()
 {
     int size = 30;
-    int c;
+    float c;
     files[0] = fopen(pelda[0], "r");
     files[1] = fopen(pelda[1], "r");
     files[2] = fopen(pelda[2], "r");
     if (files[0] && files[1] && files[2]) {
-
+        c = strtok (files[0], ",");
         fclose(files[0]);
         fclose(files[1]);
         fclose(files[2]);
@@ -24,10 +24,10 @@ int main()
     char str[] ="- This, a sample string.";
     char * pch;
     printf ("Splitting string \"%s\" into tokens:\n",str);
-    pch = strtok (str," ,");
+    pch = strtok (str, ",");
     while (pch != NULL) {
         printf ("%s\n",pch);
-        pch = strtok (NULL, " ");
+        pch = strtok (NULL, " .");
     }
     return 0;
 }
