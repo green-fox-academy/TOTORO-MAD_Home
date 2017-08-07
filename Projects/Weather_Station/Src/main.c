f652fa5eb85af699810302aa6a5a4fc7f4cc684a
Mate Lengyel <lengyel.mate@gmail.com>
2017-08-07 16:21:59 +0200

    tested on hq server with their GUI

--- Projects/Weather_Station/Src/main.c
+++ Projects/Weather_Station/Src/main.c
@@ -53,7 +53,7 @@
 /* Private macro -------------------------------------------------------------*/
 /* Private variables --------------------------------------------------------*/
 extern UART_HandleTypeDef hDiscoUart;
-uint8_t remote_ip[] = {10, 27, 99, 217};
+uint8_t remote_ip[] = {10, 27, 99, 29};
 char rx_data [500];
 char* modulename;
 float tx_data[3];
