00911bffcfcba6d5e00a796918c8b83b95bf8732
Mate Lengyel <lengyel.mate@gmail.com>
2017-08-08 11:47:39 +0200

    multiple file reading & getting the data are ok

--- Projects/ac_decoder/main.c
+++ Projects/ac_decoder/main.c
@@ -2,32 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-char pelda[15][7] = {"16.csv", "17.csv", "18.csv", "19.csv", "20.csv",
+char pelda[16][7] = {"16.csv", "17.csv", "18.csv", "19.csv", "20.csv",
                      "21.csv", "22.csv", "23.csv", "24.csv", "25.csv",
-                     "26.csv", "27.csv", "28.csv", "29.csv", "30.csv"};
+                     "26.csv", "27.csv", "28.csv", "29.csv", "30.csv",
+                     "onoff.csv"};
 
 FILE* files[15];
 
 int main()
 {
     int size = 30;
-    float c;
-    files[0] = fopen(pelda[0], "r");
-    files[1] = fopen(pelda[1], "r");
-    files[2] = fopen(pelda[2], "r");
-    if (files[0] && files[1] && files[2]) {
-        c = strtok (files[0], ",");
-        fclose(files[0]);
-        fclose(files[1]);
-        fclose(files[2]);
+    char string[50];
+    char *time;
+    char *voltage;
+    float time_1;
+    float time_2;
+    float voltage_1;
+    float voltage_2;
+    for (int i = 0; i < 3; i++) {
+        if ((files[i] = fopen(pelda[i], "r")) != NULL)
+            printf("file %s opened\n", pelda[i]);
     }
-    char str[] ="- This, a sample string.";
-    char * pch;
-    printf ("Splitting string \"%s\" into tokens:\n",str);
-    pch = strtok (str, ",");
-    while (pch != NULL) {
-        printf ("%s\n",pch);
-        pch = strtok (NULL, " .");
+    for (int i = 0; i < 3; i++) {
+        while (fgets(string, 50, (FILE*)files[i])) {
+            time = strtok (string, ",");
+            time_1 = atof(time);
+            voltage = strtok (NULL, "\n");
+            voltage_1 = atof(voltage);
+            printf("time: %f\t\t voltage: %f\n", time_1, voltage_1);
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        fclose(files[i]);
     }
     return 0;
 }
