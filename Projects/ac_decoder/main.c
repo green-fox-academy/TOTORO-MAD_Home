08e431d7ffa4456ba77d7bdd0809db1182bc1db9
Mate Lengyel <lengyel.mate@gmail.com>
2017-08-08 17:00:29 +0200

    perfect the way it is working

--- Projects/ac_decoder/main.c
+++ Projects/ac_decoder/main.c
@@ -3,16 +3,21 @@
 #include <string.h>
 #include <stdint.h>
 
-char pelda[16][7] = {"16.csv", "17.csv", "18.csv", "19.csv", "20.csv",
-                     "21.csv", "22.csv", "23.csv", "24.csv", "25.csv",
-                     "26.csv", "27.csv", "28.csv", "29.csv", "30.csv",
-                     "onoff.csv"};
+#define NUMBER_OF_FILES 16
+#define MULTIPLIER 1000
+#define EDGE_CORRECTION 40
 
-FILE* files[15];
+FILE* files[NUMBER_OF_FILES];
+
+char pelda[NUMBER_OF_FILES][10] = {"16.csv", "17.csv", "18.csv", "19.csv", "20.csv",
+                                  "21.csv", "22.csv", "23.csv", "24.csv", "25.csv",
+                                  "26.csv", "27.csv", "28.csv", "29.csv", "30.csv",
+                                  "onoff.csv"};
+
+int ups_and_downs[NUMBER_OF_FILES][320];
 
 int main()
 {
-    int ups_and_downs[320];
     char string[50];
     float time_1;
     float time_2;
@@ -21,13 +26,14 @@ int main()
     uint8_t voltage_status = 0;
     uint16_t j = 0;
 // opens the files
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < NUMBER_OF_FILES; i++) {
         if ((files[i] = fopen(pelda[i], "r")) != NULL)
             printf("file %s opened\n", pelda[i]);
     }
 // array filling algorithm
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < NUMBER_OF_FILES; i++) {
         printf("\n\n\n\n%s\n\n", pelda[i]);
+        j = 0;
         while (fgets(string, 50, files[i])) {
             time_1 = atof(strtok (string, ","));
             voltage_1 = atof(strtok (NULL, "\n"));
@@ -39,19 +45,18 @@ int main()
                     voltage_2 = atof(strtok (NULL, "\n"));
                     if (abs(voltage_2 - voltage_1) > 2.5) {
                         voltage_status = !voltage_status;
-                        ups_and_downs[j] = (time_2 - time_1) * 1000 + 40;
-                        printf("%d\n", ups_and_downs[j]);
+                        ups_and_downs[i][j] = (time_2 - time_1) * MULTIPLIER + EDGE_CORRECTION;
+                        printf("%d\n", ups_and_downs[i][j]);
                         j++;
-
                         break;
-                    }
+                    }//if
                 }//while
             }//if
         }//while
     }//for
 
 // closes the files
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < NUMBER_OF_FILES; i++) {
         fclose(files[i]);
     }
     return 0;
