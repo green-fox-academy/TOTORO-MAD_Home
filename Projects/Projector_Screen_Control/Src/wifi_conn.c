d5d1d0b4a25e9e7b77e91227bfd2f9332ed239f0
AdamBerczi <adamberczi90@gmail.com>
2017-08-18 11:33:13 +0200

    by implementing WIFI_CloseClientConnection function, client socket can be closed, but after a while reconnection is refused, program freezes in WIFI_StartServer, further investigation

--- Projects/Projector_Screen_Control/Src/wifi_conn.c
+++ Projects/Projector_Screen_Control/Src/wifi_conn.c
@@ -8,26 +8,27 @@
 #define SSID     				"A66 Guest"
 #define PASSWORD 				"Hello123"
 #define SERVER_PORT 			16000
-#define WIFI_READ_TIMEOUT 		10000
+#define WIFI_READ_TIMEOUT 		1
 #define CONNECTION_TRIAL_MAX    10
 /* Private macro -------------------------------------------------------------*/
 /* Private variables --------------------------------------------------------*/
 uint8_t mac_addr[6];
 uint8_t ip_addr[4];
-int32_t socket = 0;
+uint8_t broadcast_ip[] = {255, 255, 255, 255};
+int32_t socket;
 uint16_t datalen;
 uint8_t conn_flag;
 char command;
+extern ES_WIFIObject_t    EsWifiObj;
+extern ES_WIFI_Conn_t conn;
 
 /* Private function prototypes -----------------------------------------------*/
 void error_handling(const char *error_string, uint8_t error_code);
 /* Private functions ---------------------------------------------------------*/
-
 void send_ps_command()
 {
     /*Initialize  WIFI */
     wifi_init();
-
     while (1) {
     	/*Waiting for connection with WIFI AP */
     	printf("> Trying to connect to %s.\n", SSID);
@@ -45,21 +46,26 @@ void send_ps_command()
 			error_handling("> ERROR : es-wifi module CANNOT get IP address\n", WIFI_STATUS_ERROR);
 		}
     	/*do-while connected to WIFI AP(checking connection by pinging own IP Address) */
-		printf("Start TCP Server...\n");
-		WIFI_StartServer(socket, WIFI_TCP_PROTOCOL, "", SERVER_PORT);
-		printf("----------------------------------------- \n");
-		printf("TCP Server Started \n");
+
 		do {
+			int8_t socket = 0;
+			printf("Start TCP Server...\n");
+			while(WIFI_StartServer(socket, WIFI_TCP_PROTOCOL, "asd", SERVER_PORT) == WIFI_STATUS_OK);
+			/*printf("> es-wifi module got IP Address : %d.%d.%d.%d\n",
+					conn.RemoteIP[0],
+					conn.RemoteIP[1],
+					conn.RemoteIP[2],
+					conn.RemoteIP[3]);*/
+			printf("----------------------------------------- \n");
+			printf("TCP Server Started \n");
 			printf("receiving data...\n");
-			if (socket == -1) {
-				error_handling("> Cannot create server!\n", WIFI_STATUS_ERROR);
-			} else {
-		    	/*Trying to connect to server and sending data when connected in every 10 seconds */
-				WIFI_ReceiveData(socket, &command, sizeof(command), &datalen, WIFI_READ_TIMEOUT);
-				if (datalen > 0) {
+			/*Trying to connect to server and sending data when connected in every 10 seconds */
+			do {
+				if(datalen >0) {
 					if (command == '1') {
 						ctrl_up();
 						printf("going up\n");
+
 					} else if (command == '3') {
 						ctrl_down();
 						printf("going down\n");
@@ -70,25 +76,14 @@ void send_ps_command()
 						printf("Wrong command!\n");
 						printf("%d\n", command);
 					}
+					datalen = 0;
 				}
-
-
-		    /*When connection is lost conn_flag equals 1, else client could'not connect to server */
-				if (conn_flag == 1) {
-					printf("> Disconnected from server!\n");
-				} else {
-					conn_flag = 0;
-				}
-			}
-			printf("Closing the socket...\n");
+			} while (WIFI_ReceiveData(socket, &command, sizeof(command), &datalen, 0) == WIFI_STATUS_OK);
 			/*Closing socket when connection is lost or could'not connect */
-			if (WIFI_StopServer(socket) == WIFI_STATUS_OK) {
-				printf("----------------------------------------- \n");
-				printf("Waiting for connection...\n");
-				while (WIFI_StartServer(socket, WIFI_TCP_PROTOCOL, "", SERVER_PORT) != WIFI_STATUS_OK);
-				printf("TCP Server Started \n");
-			}
-		} while (WIFI_Ping(ip_addr, 1, 1) == WIFI_STATUS_OK);	//do-while
+			printf("Closing the socket...\n");
+			WIFI_CloseClientConnection(socket);
+			WIFI_StopServer(socket);
+		}while (ES_WIFI_IsConnected(&EsWifiObj) == 1); //do-while
 		/*If there might be a problem with pinging, disconnect from WIFI AP anyway */
 		printf("> Disconnected from WIFI!\n");
 		WIFI_Disconnect();
