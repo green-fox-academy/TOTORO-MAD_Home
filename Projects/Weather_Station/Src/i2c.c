13ee5dfa79aa7fce090e627c4a6c90ef2a0e5698
Mate Lengyel <lengyel.mate@gmail.com>
2017-09-04 13:45:16 +0200

    i2c init done

--- Projects/Weather_Station/Src/i2c.c
+++ Projects/Weather_Station/Src/i2c.c
@@ -6,9 +6,11 @@
 /* Private macro -------------------------------------------------------------*/
 /* Private variables --------------------------------------------------------*/
 GPIO_InitTypeDef GPIO_InitStruct;
-I2C_InitTypeDef I2C_InitStruct;
+I2C_HandleTypeDef i2c_handle;
 
 /* TCP client variables */
+#define I2C_TIMING		   0x0010104D // 40kHz, rise 100ns, fall 10ns
+#define I2C_ADDRESS        0b1001000
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 void i2c_init() {
@@ -20,4 +22,21 @@ void i2c_init() {
 	GPIO_InitStruct.Pull = GPIO_PULLUP;			// enable pull up resistors
 	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);					// init GPIOB
 
+	i2c_handle.Instance             = I2C1;
+	i2c_handle.Init.Timing          = I2C_TIMING;
+	i2c_handle.Init.OwnAddress1     = I2C_ADDRESS;
+	i2c_handle.Init.AddressingMode  = I2C_ADDRESSINGMODE_7BIT;
+	i2c_handle.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+	i2c_handle.Init.OwnAddress2     = 0xFF;
+	i2c_handle.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+	i2c_handle.Init.NoStretchMode   = I2C_NOSTRETCH_DISABLE;
+
+	if(HAL_I2C_Init(&i2c_handle) != HAL_OK)
+	{
+	/* Initialization Error */
+	Error_Handler();
+	}
+
+	HAL_I2CEx_ConfigAnalogFilter(&i2c_handle,I2C_ANALOGFILTER_ENABLE);
 }
+
